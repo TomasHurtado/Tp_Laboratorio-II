7316de2fcdae0b5525e1f1042e1c30176296f108
TomasHurtado <tomas221298@gmail.com>
2019-09-16 01:51:36 -0300

    Tp n°1

---
+++ Entidades/Entidades/Numero.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Numero
+    {
+        private double numero;
+
+        public Numero()
+        {
+            this.numero = 0;
+        }
+
+        public Numero(double num)
+        {
+            this.numero = num;
+
+        }
+        public Numero(string num)
+        {
+            if (double.TryParse(num, out double nume))
+            {
+                this.numero = nume;
+            }
+        }
+
+
+        public double ValidarNumero(string strNumero)
+        {
+            if (double.TryParse(strNumero, out double num))
+            {
+                return num;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        public string SetNumero
+        {
+            set
+            {
+                this.numero = ValidarNumero(value);
+            }
+        }
+
+        public  string DecimalBinario(double num)
+        {
+            string bin;
+            bin = Convert.ToString(Convert.ToByte(num), 2);
+
+
+            return bin;
+        }
+
+        public  string DecimalBinario(string num)
+        {
+            string bin;
+            if (double.TryParse(num, out double dec))
+            {
+                return bin = DecimalBinario(dec);
+            }
+            else
+            {
+                return "error";
+            }
+
+
+        }
+
+        public  string BinarioDecimal(string bin)
+        {
+            double num;
+            string numero;//*/
+            num = Convert.ToInt64(bin, 2);
+            numero = Convert.ToString(num);
+            if(double.TryParse(numero,out double numeros))
+            {
+                return numero = Convert.ToString(numeros);
+            }
+            else
+            {
+                return "error";
+            }
+
+        }
+
+        public static double operator +(Numero n1, Numero n2)
+        {
+            return n1.numero + n2.numero;
+        }
+        public static double operator -(Numero n1, Numero n2)
+        {
+            return n1.numero - n2.numero;
+        }
+        public static double operator *(Numero n1, Numero n2)
+        {
+            return n1.numero * n2.numero;
+        }
+        public static double operator /(Numero n1, Numero n2)
+        {
+            if (n2.numero == 0)
+            {
+                return double.MinValue;
+            }
+            else
+            {
+                return n1.numero / n2.numero;
+            }
+        }
+
+
+    }
+}
---
+++ Entidades/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        
+
+        public FormCalculadora()
+        {
+            InitializeComponent();
+
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.CenterToScreen();
+          
+        }
+
+        void Limpiar()
+        {
+            this.txbPriOperando.Text = "";
+            this.txbSegOperando.Text = "";
+            this.cmbSigno.Text = "";
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            double aux = 0;
+            if (double.TryParse(txbPriOperando.Text, out aux) && double.TryParse(txbSegOperando.Text, out aux) && (aux != 0 || cmbSigno.Text != "/"))
+            {
+                Entidades.Numero operador1 = new Entidades.Numero(txbPriOperando.Text);
+                Entidades.Numero operador2 = new Entidades.Numero(txbSegOperando.Text);
+                lblResultado.Text = Entidades.Calculadora.Operar(operador1, operador2, cmbSigno.Text).ToString();
+
+            }
+            else
+            {
+
+                MessageBox.Show("No se puede operar");
+                Limpiar();
+            }
+        }
+
+        private void cmbSigno_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBin_Click(object sender, EventArgs e)
+        {
+            Entidades.Numero resultado = new Entidades.Numero();
+
+            lblResultado.Text = resultado.DecimalBinario(lblResultado.Text).ToString();
+
+        }
+
+        private void btnDec_Click(object sender, EventArgs e)
+        {
+            Entidades.Numero resultado = new Entidades.Numero();
+            
+            if (lblResultado.Text == "" || lblResultado.Text == "error")
+            {
+                lblResultado.Text = "error";
+            }else
+            {
+                lblResultado.Text = resultado.BinarioDecimal(lblResultado.Text).ToString();
+            }
+        }
+
+        private void btnBin_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+    }
+}
